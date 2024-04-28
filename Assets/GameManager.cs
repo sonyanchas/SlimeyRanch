ce18a20b510cb0d6cc6e150149f9109a5bc9900d
Amari Grimes <128533553+amarikgrimes@users.noreply.github.com>
2024-04-28 19:41:04 -0400

    Update GameManager.cs

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -8,13 +8,17 @@ public class GameManager : MonoBehaviour
     [SerializeField] TMP_Text ScoreUIText;
     public int Score;
     public int Damage;
+    [SerializeField] TMP_Text HealthUIText;
+    public int Health;
+    public int Damage2;
 
     // Start is called before the first frame update
     void Start()
     {
         Score = 00000; // Initialize score as 0
         Damage = 100;
-
+        Health = 100;
+        Damage2 = 10;
     }
 
     // Update is called once per frame
@@ -22,7 +26,7 @@ public class GameManager : MonoBehaviour
     {
         
         ScoreUIText.text = Score.ToString(); // Update both strings to show score and time
-        
-       
+        HealthUIText.text = Health.ToString();
+
     }
 }
