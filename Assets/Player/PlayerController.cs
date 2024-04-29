463974858ef47baa273b32cfa0d91289ad9244a8
sonyanchas <115482749+sonyanchas@users.noreply.github.com>
2024-04-28 21:38:32 -0400

    Yeahhhhh

--- Assets/Player/PlayerController.cs
+++ Assets/Player/PlayerController.cs
@@ -15,7 +15,7 @@ public class PlayerController : MonoBehaviour
     Animator animator;
     GameManager gm;
     Collision2D collision;
-  
+    public int health = 100;
 
     void Start()
     {
@@ -86,12 +86,7 @@ public class PlayerController : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Obstacle"))
         {
-            gm.Health -= 50;
-            Debug.Log("sis got hit");
-        }
-        if (gm.Health == 0)
-        {
-            gm.Lives -= 1;
+            gm.Health -= 10;
         }
         /*if (collision.gameObject.CompareTag("Enemy"))
         {
