d5c3bc21852dfb77131401b40caa8dfee852aff9
Amari Grimes <128533553+amarikgrimes@users.noreply.github.com>
2024-04-30 21:23:40 -0400

    item

--- Assets/pickup.cs
+++ Assets/pickup.cs
@@ -2,17 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class pickup : MonoBehaviour
+public class Pickup : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    private Inventory inventory;
+    public GameObject itemButton;
+    public GameObject effect;
+
+    private void Start()
     {
-        
+        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
+        if (other.CompareTag("Player"))
+        {
+            // spawn the sun button at the first available inventory slot ! 
+
+
+            for (int i = 0; i < inventory.items.Length; i++)
+            {
+                if (inventory.items[i] == 0)
+                { // check whether the slot is EMPTY
+                    Instantiate(effect, transform.position, Quaternion.identity);
+                    inventory.items[i] = 1; // makes sure that the slot is now considered FULL
+                    Instantiate(itemButton, inventory.slots[i].transform, false); // spawn the button so that the player can interact with it
+                    Destroy(gameObject);
+                    break;
+                }
+            }
+        }
+
     }
 }
